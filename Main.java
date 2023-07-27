71ba0f494eed7fdd44190b09d59a93f17ee9e401
Sommos <Sommosgaming@gmail.com>
2023-07-27 11:45:25 +0100

    added README, gitignore & basic LatticeEncryption class

---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.math.BigInterger;
+import java.security.SecureRandom;
+
+public class LatticeEncryption {
+    // lattice parameters
+
+    // dimension of the lattice
+    private static final int N = 256;
+    // a large prime modulus 
+    private static final int Q = 12289;
+    // standard deviation for generating keys
+    private static final int STANDARD_DEVIATION = 8;
+
+    public static void main(String[] args) {
+        // generate random public and private keys
+        BigInteger[][] publicKey = generatePublicKey();
+        BigInteger[] privateKey = generatePrivateKey();
+        // message to be encrypted
+        BigInteger message = new BigInteger("1234567890");
+        // encrypt the message
+        BigInteger[] cipherText = encrypt(message, publicKey);
+        // decrypt the message
+        BigInteger decryptedMessage = decrypt(cipherText, privateKey);
+        // print results
+        System.out.println("Original Message: " + message);
+        System.out.println("Encrypted Message: " + cipherText[0]);
+        System.out.println("Decrypted Message: " + decryptedMessage);
+    }
+
+    // generates a random public key matrix
+    private static BigInteger[][] generatePublicKey() {
+        BigInteger[][] publicKey = new BigInteger[N][N];
+        SecureRandom random = new SecureRandom();
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                publicKey[i][j] = new BigInteger(STANDARD_DEVIATION, random);
+            }
+        }
+        return publicKey;
+    }
+    
+    // generates a random private key vector
+    private static BigInteger[] generatePrivateKey() {
+        BigInteger[] privateKey = new BigInteger[N];
+        SecureRandom random = new SecureRandom();
+
+        for(int i = 0; i < N; i++) {
+            privateKey[i] = new BigInteger(STANDARD_DEVIATION, random);
+        }
+        return privateKey;
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Java 3D Lattice Encryption
+
+This program allows the user to encrypt and decrypt data using a 3D lattice to combat Quantum computers.
+
+# Contribution
+
+If you'd like to contribute to `java_3d_lattice_encryption` please submit a pull-request on a feature branch.
+
+# Installing
+
+Clone the repo:
+
+    git clone http://github.com/Sommos/java_3d_lattice_encryption
+    cd java_3d_lattice_encryption
